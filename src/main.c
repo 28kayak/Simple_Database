f0ef436d188533e648899ec358a932c31bcd3052
kayak28 <0246kaya>
2021-09-30 01:26:19 +0900

    refactor to *.h files and delete the commend out; this close #1

--- src/main.c
+++ src/main.c
@@ -6,36 +6,6 @@
 #include "statement.h"
 #include "input_buffer.h"
 
-/*enum MetaCommandResult_t{
-    META_COMMAND_SUCCESS,
-    META_COMMAND_UNRECOGNIZED_COMMAND
-};
-typedef enum MetaCommandResult_t MetaCommandResult;
-
-enum PrepareResult_t{
-    PREPARE_SUCCESS,
-    PREPARE_UNRECOGNIZED_COMMAND
-};
-typedef enum PrepareResult_t PrepareResult; */
-
-/*enum StatementType_t {
-    STATEMENT_INSERT,
-    STATEMENT_SELECT
-};
-typedef enum StatementType_t StatementType;
-
-struct Statement_t {
-    StatementType type;
-};
-typedef struct Statement_t Statement; */
-
-/*struct InputBuffer_t{
-    char* buffer;
-    size_t buffer_length;
-    ssize_t input_length;
-};
-typedef struct InputBuffer_t InputBuffer;*/
-
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
     /*
      * NOTE: use strncmp to compare because insert keyword will be followed by data
