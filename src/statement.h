3ccc30db72255a17787148d85693a2d5d9c9f78e
kayak28 <0246kaya>
2021-09-30 01:59:17 +0900

    part3: add data structure for ROW to store parsed arguments

--- src/statement.h
+++ src/statement.h
@@ -4,7 +4,8 @@
 
 #ifndef SIMPLE_BATABASE_STATEMENT_H
 #define SIMPLE_BATABASE_STATEMENT_H
-
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255 
 #endif //SIMPLE_BATABASE_STATEMENT_H
 
 enum StatementType_t {
@@ -13,8 +14,15 @@ enum StatementType_t {
 };
 typedef enum StatementType_t StatementType;
 
+typedef struct{
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+} Row;
+
 struct Statement_t {
     StatementType type;
+    Row row_to_insert; // only used by insert statement
 };
 typedef struct Statement_t Statement;
 
